e9b34b0a363dd675a690a5ababf75d9ef04367a6
api <api@api-laptop.(none)>
2011-04-30 10:13:40 -0500

    Add Schema

--- slides/Models/conf.js
+++ slides/Models/conf.js
@@ -2,8 +2,8 @@ var conf = {
 	url: 'flame.mongohq.com',
 	port: 27038,
 	user: 'Siedrix',
-	password: 'RusiaWithLove',
-	database: 'slides'
+	password: 'Tailandia1#',
+	database: 'ApiHackaton'
 }
 
 var url = "mongodb://"+conf.user+":"+conf.password+"@"+conf.url+":"+conf.port+"/"+conf.database
